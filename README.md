b59df568c0942ba6a8116babe74494fd957f1582
Matt Hoffner <matthoffner@gmail.com>
2023-04-07 18:52:30 -0700

    full page screenshots

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # scaipe
 
-[puppeteer-screen-recorder](https://www.npmjs.com/package/puppeteer-screen-recorder) in an iframe connected to [Dalai](https://github.com/cocktailpeanut/dalai).
-
 https://scaipe-n325xsyjoq-uc.a.run.app/index.html
--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
-const fs = require('fs');
-const { PuppeteerScreenRecorder } = require('puppeteer-screen-recorder');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const readability = require('@mozilla/readability');
-// const Dalai = require('dalai');
-const { homedir } = require('os');
-
-
-// const dalai = new Dalai('/root/dalai/alpaca/main.cpp');
 
 const app = express();
 
@@ -36,71 +26,22 @@ app.get('/index.html', async (req, res) => {
   file.pipe(res);
 });
 
+
 app.get('/scaipe', async (req, res) => {
-  const browser = await puppeteer.launch(defaultArgs);
-  const page = await browser.newPage();
-  const recorder = new PuppeteerScreenRecorder(page);
-  const videoPath = `./${req.query.url.replace('https://', '').replaceAll('/', '')}.mp4`; 
-  
-  await recorder.start(videoPath);
-  try {
+    const browser = await puppeteer.launch(defaultArgs);
+    const page = await browser.newPage();
     await page.setViewport({
-      width: 768,
-      height: 1024,
-      deviceScaleFactor: 1,
+      width: req.query.width || 768,
+      height: 1280,
+      deviceScaleFactor: 1
     });
     await page.goto(req.query.url, { waitUntil: 'networkidle2' });
-    // await page.waitForFunction(() => document.readyState === "complete");
-  } catch (err) {
-    console.log(err);
-    res.send(err);
-    return;
-  }
-  let videoStat;
-  let fileSize;
-  try {
-    videoStat = fs.statSync(videoPath);
-    fileSize = videoStat.size;
-  } catch (err) {
-    fs.closeSync(fs.openSync(videoPath, 'a'))
-  }
-  const videoRange = req.headers.range;
-  if (videoRange && fileSize) {
-      const parts = videoRange.replace(/bytes=/, "").split("-");
-      const start = parseInt(parts[0], 10);
-      const end = parts[1]
-          ? parseInt(parts[1], 10)
-          : fileSize-1;
-      const chunksize = (end-start) + 1;
-      const file = fs.createReadStream(videoPath, {start, end});
-      const header = {
-          'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-          'Accept-Ranges': 'bytes',
-          'Content-Length': chunksize,
-          'Content-Type': 'video/mp4'
-      };
-      res.writeHead(206, header);
-      file.pipe(res);
-  } else {
-      const body = await page.evaluate(() => {
-        return document.querySelector("body").innerHTML;
-      });
-      const { document } = new JSDOM(body).window;
-      const article = new readability.Readability(document).parse();
-      await recorder.stop();
-      await browser.close();
-      updatedFile = fs.statSync(videoPath);
-      
-
-      const head = {
-          'Content-Length': updatedFile.size,
-          'Content-Type': 'video/mp4',
-          'Extracted-Text': Buffer.from(article.textContent).toString('base64')
-      };
-      res.writeHead(200, head);
-      fs.createReadStream(videoPath).pipe(res);
-  }
+    const image = await page.screenshot({fullPage: true});
+    await browser.close();
+    res.writeHead(200, {'Content-Type': 'image/png'});
+    res.end(image);
 });
+
 
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
